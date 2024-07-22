1913f518a2bc5082e97b716dd8846fd3a07264aa
Janschuri <124630782+Janschuri@users.noreply.github.com>
2024-07-22 16:14:02 +0200

    check for fake event

--- lunaticlib-platform/lunaticlib-platform-bukkit/src/main/java/de/janschuri/lunaticlib/platform/bukkit/util/EventUtils.java
+++ lunaticlib-platform/lunaticlib-platform-bukkit/src/main/java/de/janschuri/lunaticlib/platform/bukkit/util/EventUtils.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.ClickType;
@@ -17,16 +18,23 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class EventUtils {
+
+    private static List<Event> fakeEvents = new ArrayList<>();
 
     private EventUtils() {
     }
 
     public static boolean isAllowedViewChest(Player player, Block chest) {
         PlayerInteractEvent event = new PlayerInteractEvent(player, Action.RIGHT_CLICK_BLOCK, new ItemStack(Material.AIR), chest, BlockFace.UP);
+        fakeEvents.add(event);
         Bukkit.getPluginManager().callEvent(event);
         boolean allowed = !event.isCancelled();
         event.setCancelled(true);
+        fakeEvents.remove(event);
         return allowed;
     }
 
@@ -39,9 +47,11 @@ public final class EventUtils {
         player.setItemOnCursor(cursor);
 
         InventoryClickEvent event = new InventoryClickEvent(view, InventoryType.SlotType.CONTAINER, 0, ClickType.LEFT, InventoryAction.PICKUP_ALL);
+        fakeEvents.add(event);
         Bukkit.getPluginManager().callEvent(event);
         boolean allowed = !event.isCancelled();
         event.setCancelled(true);
+        fakeEvents.remove(event);
         return allowed;
     }
 
@@ -54,17 +64,25 @@ public final class EventUtils {
         player.setItemOnCursor(cursor);
 
         InventoryClickEvent event = new InventoryClickEvent(view, InventoryType.SlotType.CONTAINER, 0, ClickType.RIGHT, InventoryAction.PLACE_ALL);
+        fakeEvents.add(event);
         Bukkit.getPluginManager().callEvent(event);
         boolean allowed = !event.isCancelled();
         event.setCancelled(true);
+        fakeEvents.remove(event);
         return allowed;
     }
 
     public static boolean isAllowedPlaceBlock(Player player, Block block) {
         BlockPlaceEvent event = new BlockPlaceEvent(block, block.getState(), block, new ItemStack(block.getType()), player, false, EquipmentSlot.HAND);
+        fakeEvents.add(event);
         Bukkit.getPluginManager().callEvent(event);
         boolean allowed = !event.isCancelled();
         event.setCancelled(true);
+        fakeEvents.remove(event);
         return allowed;
+    }
+
+    public static boolean isFakeEvent(Event event) {
+        return fakeEvents.contains(event);
     }
 }
