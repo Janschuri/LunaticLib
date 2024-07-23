72f39f25dced3eb6d802af9a8cb793f2724a822f
Janschuri <124630782+Janschuri@users.noreply.github.com>
2024-07-23 18:56:17 +0200

    removed block utils

--- lunaticlib-platform/lunaticlib-platform-bukkit/src/main/java/de/janschuri/lunaticlib/platform/bukkit/util/BukkitUtils.java
+++ lunaticlib-platform/lunaticlib-platform-bukkit/src/main/java/de/janschuri/lunaticlib/platform/bukkit/util/BukkitUtils.java
@@ -8,18 +8,6 @@ import java.util.UUID;
 
 public class BukkitUtils {
 
-    public static long serializeCoords(Location location) {
-        return (long) location.getBlockX() << 38 | (long) location.getBlockY() << 26 | (long) location.getBlockZ();
-    }
-
-    public static Location deserializeCoords(long coords, UUID worldUUID) {
-        int x = (int) (coords >> 38);
-        int y = (int) (coords >> 26 & 0xFFF);
-        int z = (int) (coords & 0xFFF);
-        World world = Bukkit.getWorld(worldUUID);
-        return new Location(world, x, y, z);
-    }
-
     public static boolean isInRange(Location location, Location otherLocation, double range) {
         return location.distance(otherLocation) <= range;
     }
