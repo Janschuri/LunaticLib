bd6facd640934be8e67e472398b065d5a16a091e
Janschuri <jannis.schuricht@gmail.com>
2026-02-20 20:35:51 +0100

    feat: add GitHub Actions workflow and update Gradle configuration

--- lunaticlib-proxyrequests-paper/build.gradle.kts
+++ lunaticlib-proxyrequests-paper/build.gradle.kts
@@ -1,17 +1,3 @@
-plugins {
-    id("java-library")
-    id("maven-publish")
-}
-val lunaticlibVersion: String by rootProject.extra
-
-group = "de.janschuri"
-version = "$lunaticlibVersion"
-
-repositories {
-    mavenLocal()
-    mavenCentral()
-}
-
 dependencies {
     api(project(":lunaticlib-sender-paper"))
     api(project(":lunaticlib-proxyrequests"))
@@ -20,29 +6,4 @@ dependencies {
     compileOnly(project(":lunaticlib-inventorygui-paper"))
     compileOnly(libs.io.papermc.paper.paper.api)
     compileOnly(libs.com.github.milkbowl.vaultapi)
-}
-
-val targetJavaVersion = 17
-java {
-    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
-    sourceCompatibility = javaVersion
-    targetCompatibility = javaVersion
-    if (JavaVersion.current() < javaVersion) {
-        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
-    }
-}
-
-tasks.withType<JavaCompile>().configureEach {
-    options.encoding = "UTF-8"
-    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
-        options.release.set(targetJavaVersion)
-    }
-}
-
-
-
-publishing {
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
-    }
 }
